0e4469c30958d1d9d73e67f2bdfbe64fa3413bff
Sebastiano Guzzone <47942024+LilloBarilotto@users.noreply.github.com>
2021-06-14 11:56:31 +0200

    Add files via upload

--- Exam20200904_Timesheet/src/timesheet/Worker.java
+++ Exam20200904_Timesheet/src/timesheet/Worker.java
@@ -2,16 +2,48 @@ package timesheet;
 
 public class Worker {
 	
-	public String name;
-	public String surname;
-	public String workerID;
-	public String profileID;
+	private String name;
+	private String surname;
+	private String profileID;
 	
-	public Worker(String name, String surname, String workerID, String profileID) {
-		this.name= name;
-		this.surname= surname;
-		this.workerID = workerID;
-		this.profileID= profileID;
+	
+	public Worker(String name, String surname, String profileID)  {
+		this.name=name;
+		this.surname=surname;
+		this.profileID=profileID;
 	}
+
+
+	public String getName() {
+		return name;
+	}
+
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+
+	public String getSurname() {
+		return surname;
+	}
+
+
+	public void setSurname(String surname) {
+		this.surname = surname;
+	}
+
+
+	public String getProfileID() {
+		return profileID;
+	}
+
+
+	public void setProfileID(String profileID) {
+		this.profileID = profileID;
+	}
+
+	
+	
 	
 }
